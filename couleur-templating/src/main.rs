7571bdf6b720b1f1a5b59c62cb220666baa62452
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-01 07:23:26 +0000

    does not seem to work at first stab

--- couleur-templating/src/main.rs
+++ couleur-templating/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+pub use couleur_templating::{
+    Color,
+    ColorRgb,
+    Contrast,
+    Definition,
+    Error,
+    Layer,
+    Markup,
+    Node,
+    Reset,
+    Result,
+    RgbHex,
+    Text,
+    Unhandled,
+    parse_tokens,
+};
+
+fn main() -> Result<()>{
+    let input = "{color:#FB6B1D}".to_string();
+    let node = parse_tokens(&input)?;
+
+    println!("node: {node:#?}");
+    Ok(())
 }
