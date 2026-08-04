ad6c1b8cc61c4d443eeebafe3324a8899eb73419
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-04 07:17:19 +0000

    parse_u8: passes

--- src/templating.rs
+++ src/templating.rs
@@ -3,9 +3,9 @@ use crate::{Color, Contrast, Error, Layer, Reset, Result};
 use std::{collections::HashMap, str, str::FromStr};
 
 use winnow::{
-    ascii::{digit1, float, hex_digit1},
+    ascii::{digit1, float, hex_digit1, dec_uint},
     combinator::{alt, cut_err, delimited, preceded, repeat, separated, separated_pair, seq, terminated},
-    error::{AddContext, ErrMode, ParserError, StrContext},
+    error::{AddContext, ContextError, ErrMode, ParserError, StrContext},
     prelude::*,
     token::{any, none_of, take, take_while},
 };
@@ -62,6 +62,24 @@ fn color<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(in
     .context(StrContext::Expected("rgb color".into()))
     .parse_next(input)
 }
+
+fn parse_u8<'i, E: ParserError<Stream<'i>> + std::fmt::Debug + AddContext<Stream<'i>, StrContext>>(
+    input: &mut Stream<'i>,
+) -> ModalResult<u8, E> {
+    dec_uint::<Stream<'i>, u8, ErrMode<E>>(input)
+    // let val =
+    //     repeat(0..2, digit1::<Stream<'i>, E>).fold(|| String::new(), |acc, next| format!("{acc}{next}")).parse_next(input).unwrap();
+
+    // match val.parse::<u8>() {
+    //     Ok(val) => Ok(val),
+    //     Err(error) => {
+    //         // let mut err = ContextError::new();
+    //         // err.push(StrContext::Label("digit"));
+    //         // err.push(StrContext::Expected("number between 0 and 255".into()));
+    //         Err(ErrMode::Cut(ParserError::from_input(input)))
+    //     }
+    // }
+}
 fn parse_triple<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
     input: &mut Stream<'i>,
 ) -> ModalResult<(String, String, String), E> {
@@ -113,52 +131,59 @@ mod tests {
     ///    - [ ] 3.1 IMPORTANT: take note of this particular test spec and make a reference to it when writing tests for template rendering: "Hello" must be colored with #E83B3B while " World" must be colored with #68BBBB because that's its *"web"* contrast color.
     use super::*;
 
-    use winnow::error::ContextError as Error;
-    type Result<T> = std::result::Result<T, Error>;
+    use winnow::error::{ContextError, ErrMode};
+    type Result<T> = std::result::Result<T, ContextError>;
     use std::str::FromStr;
 
     #[test]
-    fn test_parse_triple() {
-        assert_eq!(
-            parse_triple::<Error>.parse_peek("127,255,355,"),
-            Ok(("", ("127".to_string(), "255".to_string(), "355".to_string())))
-        );
-        assert_eq!(
-            parse_triple::<Error>.parse_peek("127,255,123"),
-            Ok(("", ("127".to_string(), "255".to_string(), "123".to_string())))
-        );
+    fn test_parse_u8() {
+        assert_eq!(parse_u8::<ContextError>.parse_peek("127"), Ok(("", 127u8)));
+        assert_eq!(parse_u8::<ContextError>.parse_peek("255"), Ok(("", 255u8)));
+        assert_eq!(parse_u8::<ContextError>.parse_peek("300"), Err(ErrMode::Backtrack(ContextError::new())));
     }
 
     #[test]
+    fn test_parse_triple_to_string() {
+        assert_eq!(parse_triple::<ContextError>.parse_peek("127,255,71,"), Ok(("", ("127".to_string(), "255".to_string(), "71".to_string()))));
+        assert_eq!(parse_triple::<ContextError>.parse_peek("127,255,63"), Ok(("", ("127".to_string(), "255".to_string(), "63".to_string()))));
+    }
+
+    // #[test]
+    // fn test_parse_triple() {
+    //     assert_eq!(parse_triple::<ContextError>.parse_peek("127,255,71,"), Ok(("", (127u8, 255u8, 71u8))));
+    //     assert_eq!(parse_triple::<ContextError>.parse_peek("127,255,63"), Ok(("", (127u8, 255u8, 63u8))));
+    // }
+
+    #[test]
     fn test_parse_reset() -> Result<()> {
-        assert_eq!(reset::<Error>.parse_peek("{reset}"), Ok(("", Reset::default())));
-        assert_eq!(parse_node::<Error>.parse_peek("{reset}"), Ok(("", Node::Reset(Reset::default()))));
+        assert_eq!(reset::<ContextError>.parse_peek("{reset}"), Ok(("", Reset::default())));
+        assert_eq!(parse_node::<ContextError>.parse_peek("{reset}"), Ok(("", Node::Reset(Reset::default()))));
         Ok(())
     }
 
     #[test]
     fn test_parse_color_rgb_hex_prefixed_hash() -> Result<()> {
-        assert_eq!(color::<Error>.parse_peek("{color:#F04F78}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
+        assert_eq!(color::<ContextError>.parse_peek("{color:#F04F78}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
         assert_eq!(
-            parse_node::<Error>.parse_peek("{color:#F04F78}"),
+            parse_node::<ContextError>.parse_peek("{color:#F04F78}"),
             Ok(("", Node::Color("#F04F78".parse::<Color>().expect("parse rgb color"))))
         );
         Ok(())
     }
     #[test]
     fn test_parse_color_rgb_hex() -> Result<()> {
-        assert_eq!(color::<Error>.parse_peek("{color:F04F78}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
+        assert_eq!(color::<ContextError>.parse_peek("{color:F04F78}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
         assert_eq!(
-            parse_node::<Error>.parse_peek("{color:F04F78}"),
+            parse_node::<ContextError>.parse_peek("{color:F04F78}"),
             Ok(("", Node::Color("#F04F78".parse::<Color>().expect("parse rgb color"))))
         );
         Ok(())
     }
     // #[test]
     // fn test_parse_color_rgb_u8_triple() -> Result<()> {
-    //     assert_eq!(color::<Error>.parse_peek("{color:240,79,120}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
+    //     assert_eq!(color::<ContextError>.parse_peek("{color:240,79,120}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
     //     assert_eq!(
-    //         parse_node::<Error>.parse_peek("{color:240,79,120}"),
+    //         parse_node::<ContextError>.parse_peek("{color:240,79,120}"),
     //         Ok(("", Node::Color("#F04F78".parse::<Color>().expect("parse rgb color"))))
     //     );
     //     Ok(())
