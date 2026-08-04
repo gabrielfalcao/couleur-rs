acaa7b258e1c9c23a4a18a1adcee673fd3763bc5
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-04 04:15:03 +0000

    done: item #2 of first round of TDD

--- src/templating.rs
+++ src/templating.rs
@@ -1,16 +1,12 @@
 // use crate::{Error, Result};
 use crate::{Color, Contrast, Error, Layer, Reset, Result};
-use std::collections::HashMap;
-use std::str;
+use std::{collections::HashMap, str, str::FromStr};
 
-use winnow::prelude::*;
 use winnow::{
-    ascii::float,
-    combinator::alt,
-    combinator::cut_err,
-    combinator::{delimited, preceded, separated_pair, terminated},
-    combinator::{repeat, separated},
-    error::{AddContext, ParserError, StrContext},
+    ascii::{float, hex_digit1},
+    combinator::{alt, cut_err, delimited, preceded, repeat, separated, separated_pair, terminated},
+    error::{AddContext, ErrMode, ParserError, StrContext},
+    prelude::*,
     token::{any, none_of, take, take_while},
 };
 
@@ -19,32 +15,42 @@ pub(crate) type Stream<'i> = &'i str;
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Node {
     Reset(Reset),
+    Color(Color),
 }
 impl From<Reset> for Node {
-    fn from(reset: Reset ) -> Node {
+    fn from(reset: Reset) -> Node {
         Node::Reset(reset)
     }
 }
-pub fn parse_node<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
-    input: &mut Stream<'i>,
-) -> ModalResult<Node, E> {
-    // `alt` combines the each value parser. It returns the result of the first
-    // successful parser, or an error
-    alt((
-        reset.map(Node::Reset),
-    ))
-    .parse_next(input)
+impl From<Color> for Node {
+    fn from(color: Color) -> Node {
+        Node::Color(color)
+    }
+}
+pub fn parse_node<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(input: &mut Stream<'i>) -> ModalResult<Node, E> {
+    alt((reset.map(Node::Reset), color.map(Node::Color))).parse_next(input)
 }
 
-
-fn reset<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
-    input: &mut Stream<'i>,
-) -> ModalResult<Reset, E> {
+fn reset<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(input: &mut Stream<'i>) -> ModalResult<Reset, E> {
+    preceded('{', terminated("reset".value(Reset::default()), '}')).context(StrContext::Expected("reset".into())).parse_next(input)
+}
+fn color<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(input: &mut Stream<'i>) -> ModalResult<Color, E> {
     preceded(
         '{',
-        cut_err(terminated("reset".value(Reset::default()), '}')),
+        terminated(
+            preceded(
+                "color:",
+                preceded(
+                    "#",
+                    repeat(0..5, hex_digit1)
+                        .fold(|| String::new(), |acc, item| format!("{acc}{item}"))
+                        .map(|string| string.parse::<Color>().expect("6 hex digits")),
+                ),
+            ),
+            '}',
+        ),
     )
-    .context(StrContext::Expected("reset".into()))
+    .context(StrContext::Expected("rgb color".into()))
     .parse_next(input)
 }
 
@@ -55,7 +61,7 @@ mod tests {
     /// ### first set of red -> green -> refactor rounds:
     ///
     ///  - [x] parse "{reset}" to `Node::Reset` *DONE:
-    ///  - [ ] 2. parse "{color:#F04F78}" to `Node::Color(crate::Color)`
+    ///  - [x] 2. parse "{color:#F04F78}" to `Node::Color(crate::Color)`
     ///  - [ ] 3. parse "{color:F04F78}" to `Node::Color(crate::Color)`
     ///  - [ ] 4. parse "{color:240,79,120}" to `Node::Color(crate::Color)`
     ///  - [ ] 5. parse "{color:240,  79, 120 , }" to `Node::Color(crate::Color)`
@@ -84,14 +90,24 @@ mod tests {
     ///  - [ ] 3. parse "{color:#E83B3B}Hello{color:#E83B3B%contrast:web} World" to `Node::Array(vec![Node::Color(Node::Color("#E83B3B".parse<crate::Color>()?)), Node::Text("Hello"), Node::ContrastedColor(Node::Contrast(Contrast::Web), Node::Color("#E83B3B".parse<crate::Color>()?)), Node::Text(" World")])`
     ///    - [ ] 3.1 IMPORTANT: take note of this particular test spec and make a reference to it when writing tests for template rendering: "Hello" must be colored with #E83B3B while " World" must be colored with #68BBBB because that's its *"web"* contrast color.
     use super::*;
-    use k9::assert_equal;
+
     use winnow::error::ContextError as Error;
     type Result<T> = std::result::Result<T, Error>;
-
+    use std::str::FromStr;
     #[test]
     fn test_parse_reset() -> Result<()> {
-        assert_equal!(reset::<Error>.parse_peek("{reset}"), Ok(("", Reset::default())));
-        assert_equal!(parse_node::<Error>.parse_peek("{reset}"), Ok(("", Node::Reset(Reset::default()))));
+        assert_eq!(reset::<Error>.parse_peek("{reset}"), Ok(("", Reset::default())));
+        assert_eq!(parse_node::<Error>.parse_peek("{reset}"), Ok(("", Node::Reset(Reset::default()))));
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_color_rgb() -> Result<()> {
+        assert_eq!(color::<Error>.parse_peek("{color:#F04F78}"), Ok(("", "#F04F78".parse::<Color>().expect("parse rgb color"))));
+        assert_eq!(
+            parse_node::<Error>.parse_peek("{color:#F04F78}"),
+            Ok(("", Node::Color("#F04F78".parse::<Color>().expect("parse rgb color"))))
+        );
         Ok(())
     }
 }
