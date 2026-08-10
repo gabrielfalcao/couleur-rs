90824829a5b9d1a3a9a5d7e5d90348a96b7897c1
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-10 00:36:02 +0000

    restore TDD spec

--- src/templating.rs
+++ src/templating.rs
@@ -325,3 +325,38 @@ pub fn render<
     let items = render_nodes([nodes].into_iter()).to_string();
     Ok(render::<E>(input)?)
 }
+/// # TDD TODO:
+///
+/// ### first set of red -> green -> refactor rounds:
+///
+///  - [x] parse "{reset}" to `Node::Reset` *DONE:
+///  - [x] 2. parse "{color:#F04F78}" to `Node::Color(crate::Color)`
+///  - [x] 3. parse "{color:F04F78}" to `Node::Color(crate::Color)`
+///  - [x] 4. parse "{color:240,79,120}" to `Node::Color(crate::Color)`
+///  - [x] 5. parse "{color:240,  79, 120 , }" to `Node::Color(crate::Color)`
+///
+/// ### second set of red -> green -> refactor rounds:
+///
+///  - [x] 1. parse "hello {reset} world" to `Node::Array(vec![Node::Text("hello "), Node::Reset, Node::Text(" world")])`
+///  - [x] 2. parse "{color:#4D9BE6}hello {color:#91DB69}world{reset}" to something like `Node::Array(vec![Node::Color("#4D9BE6".parse::<crate::Color>()?), Node::Text("hello "), Node::Color("#91DB69".parse::<crate::Color>()?), Node::Text("world"), Node::Reset])`
+///
+/// ### third set of red -> green -> refactor rounds:
+///
+///  - [x] 1. parse "{layer:bg}" to `Node::Layer(crate::Layer::BG)`
+///  - [x] 2. parse "{layer:fg}" to `Node::Layer(crate::Layer::FG)`
+///  - [x] 3. parse "{color:#F9C22B@layer:bg}" to something like `Node::RenderableColor(Node::Layer(crate::Layer::FG), Node::Color("#F9C22B".parse<crate::Color>()?))`
+///
+/// ### fourth set of red -> green -> refactor rounds:
+///
+///  - [x] 1. parse "{contrast:*VARIANT*}" for each of **variant** of the `crate::Contrast` enum, that is:
+///    - [x] 1.1 "{contrast:none}" should parse to `Node::Contrast(Contrast::None)`
+///    - [x] 1.2 "{contrast:read}" should parse to `Node::Contrast(Contrast::Read)`
+///    - [x] 1.3 "{contrast:high_bit}" should parse to `Node::Contrast(Contrast::HighBit)`
+///    - [x] 1.4 "{contrast:harmonic}" should parse to `Node::Contrast(Contrast::Harmonic)`
+///    - [x] 1.5 "{contrast:web}" should parse to `Node::Contrast(Contrast::Web)`
+///
+///  - [x] 2. parse "{color:#E83B3B%contrast:web}" to something like `Node::RenderableColor(Node::Contrast(Contrast::Web), Node::Color("#E83B3B".parse<crate::Color>()?))`
+///  - [x] 3. parse "{color:#E83B3B@layer:bg%contrast:web}" to something like `Node::RenderableColor(Node::Color("#E83B3B".parse<crate::Color>()?, Node::Contrast(Contrast::Web), Node::Layer(Layer::BG), ))`
+///  - [x] 4. parse "{color:#E83B3B}Hello{color:#E83B3B%contrast:web} World" to something like `Node::Array(vec![Node::Color(Node::Color("#E83B3B".parse<crate::Color>()?)), Node::Text("Hello"), Node::RenderableColor(Node::Contrast(Contrast::Web), Node::Color("#E83B3B".parse<crate::Color>()?)), Node::Text(" World")])`
+///    - [ ] 4.1 IMPORTANT: take note of this particular test spec and make a reference to it when writing tests for template rendering: "Hello" must be colored with #E83B3B while " World" must be colored with #68BBBB because that's its *"web"* contrast color.
+/// 4.0 IMPORTANT: take note of this particular test spec and make a reference to it when writing tests for template rendering: "Hello" must be colored with #E83B3B while " World" must be colored with #68BBBB because that's its *"web"* contrast color.
