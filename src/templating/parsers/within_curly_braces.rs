041bc42592b0ae209c7f69e64c2e57e2713a8347
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-13 20:48:39 +0000

    reduce duplicate code, but still got 4 errors

--- src/templating/parsers/within_curly_braces.rs
+++ src/templating/parsers/within_curly_braces.rs
@@ -10,22 +10,43 @@ use super::*;
 use crate::{Color, Contrast, Layer, Stream};
 
 #[cfg_attr(feature = "tracing", instrument)]
+pub fn rgb_color_declaration<
+    'i,
+    E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>,
+>(
+    input: &mut Stream<'i>,
+) -> ModalResult<Contrast, E> {
+    #[cfg(feature = "tracing")]
+    span!(Level::TRACE, "input", input);
+    alt((parse_rgb_triple, preceded("#", parse_rgb_hex), parse_rgb_hex)).parse_next(input)
+}
+
+#[cfg_attr(feature = "tracing", instrument)]
+pub fn contrast_alternatives<
+    'i,
+    E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>,
+>(
+    input: &mut Stream<'i>,
+) -> ModalResult<Contrast, E> {
+    #[cfg(feature = "tracing")]
+    span!(Level::TRACE, "input", input);
+    alt((
+        "none".value(Contrast::None),
+        "read".value(Contrast::Read),
+        "high_bit".value(Contrast::HighBit),
+        "harmonic".value(Contrast::Harmonic),
+        "web".value(Contrast::Web),
+    ))
+    .parse_next(input)
+}
+
+#[cfg_attr(feature = "tracing", instrument)]
 pub fn parse_contrast<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
     input: &mut Stream<'i>,
 ) -> ModalResult<Contrast, E> {
     #[cfg(feature = "tracing")]
     span!(Level::TRACE, "input", input);
-    preceded(
-        "contrast:",
-        alt((
-            "none".value(Contrast::None),
-            "read".value(Contrast::Read),
-            "high_bit".value(Contrast::HighBit),
-            "harmonic".value(Contrast::Harmonic),
-            "web".value(Contrast::Web),
-        )),
-    )
-    .parse_next(input)
+    preceded("contrast:", contrast_alternatives::<E>).parse_next(input)
 }
 #[cfg_attr(feature = "tracing", instrument)]
 pub fn parse_color<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
@@ -35,23 +56,11 @@ pub fn parse_color<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrCo
     span!(Level::TRACE, "input", input);
     alt((preceded(
         "contrasted_color:",
-        (
-            alt((parse_rgb_triple, preceded("#", parse_rgb_hex), parse_rgb_hex)),
-            preceded(
-                ":",
-                alt((
-                    "none".value(Contrast::None),
-                    "read".value(Contrast::Read),
-                    "high_bit".value(Contrast::HighBit),
-                    "harmonic".value(Contrast::Harmonic),
-                    "web".value(Contrast::Web),
-                )),
-            ),
-        )
-            .map(|(color, contrast): (Color, Contrast)| contrast.of(color)),
-        preceded("color:", alt((parse_rgb_triple, preceded("#", parse_rgb_hex), parse_rgb_hex))),
+        (rgb_color_declaration::<E>, preceded(":", contrast_alternatives::<E>))
+            .map(|(color, contrast): (Color, Contrast)| contrast.of(color))
+            .parse_next(input),
+        preceded("color:", rgb_color_declaration::<E>).parse_next(input),
     )))
-    .parse_next(input)
 }
 #[cfg_attr(feature = "tracing", instrument)]
 pub fn parse_layer<'i, E: ParserError<Stream<'i>> + AddContext<Stream<'i>, StrContext>>(
