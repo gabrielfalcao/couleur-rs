61fad6d226f9491538fd3fd90c1b694c6f3af9ab
Gabriel Falcão <gabrielteratos@gmail.com>
2026-08-15 15:21:11 +0000

    preliminary list of colors

---
+++ src/color_naming/mod.rs
@@ -0,0 +1,2 @@
+#[doc(hidden)] pub mod colori;
+#[doc(inline)] pub use colori::COLORI_NAMES;
---
+++ src/color_naming/rust_colornames.rs
@@ -0,0 +1,92 @@
+pub const RUST_COLORNAMES_NAMES: [(&'static str, (u8, u8, u8)); 152] = [
+    (
+        //name: Black
+        "black" // RGB value for "black"
+        0, 0, 0,
+    ),
+    (
+        //name: White
+        "white" // RGB value for "white"
+        0, 0, 0,
+    ),
+    (
+        //name: Red
+        "red" // RGB value for "red"
+        0, 0, 0,
+    ),
+    (
+        //name: Lime
+        "lime" // RGB value for "lime"
+        0, 0, 0,
+    ),
+    (
+        //name: Blue
+        "blue" // RGB value for "blue"
+        0, 0, 0,
+    ),
+    (
+        //name: Yellow
+        "yellow" // RGB value for "yellow"
+        0, 0, 0,
+    ),
+    (
+        //name: Cyan
+        "cyan" // RGB value for "cyan"
+        0, 0, 0,
+    ),
+    (
+        //name: Aqua
+        "aqua" // RGB value for "aqua"
+        0, 0, 0,
+    ),
+    (
+        //name: Magenta
+        "magenta" // RGB value for "magenta"
+        0, 0, 0,
+    ),
+    (
+        //name: Fuchsia
+        "fuchsia" // RGB value for "fuchsia"
+        0, 0, 0,
+    ),
+    (
+        //name: Silver
+        "silver" // RGB value for "silver"
+        0, 0, 0,
+    ),
+    (
+        //name: Gray
+        "gray" // RGB value for "gray"
+        80, 80, 80,
+    ),
+    (
+        //name: Maroon
+        "maroon" // RGB value for "maroon"
+        80, 0, 0,
+    ),
+    (
+        //name: Olive
+        "olive" // RGB value for "olive"
+        80, 80, 0,
+    ),
+    (
+        //name: Green
+        "green" // RGB value for "green"
+        0, 80, 0,
+    ),
+    (
+        //name: Purple
+        "purple" // RGB value for "purple"
+        80, 0, 80,
+    ),
+    (
+        //name: Teal
+        "teal" // RGB value for "teal"
+        0, 80, 80,
+    ),
+    (
+        //name: Navy
+        "navy" // RGB value for "navy"
+        0, 0, 80,
+    ),
+];
